f44f5d605a94bd522bb37bd3481f091506436eb2
ian038 <ianphua3@gmail.com>
2022-01-17 16:41:01 -0500

    Completed app

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,19 @@
 import React, { useState } from 'react'
 import './App.css';
-import { gAbi, greeterAddress } from './utils/constants';
+import { gAbi, greeterAddress, tokenAddress, tAbi } from './utils/constants';
 import { ethers } from 'ethers'
 
 function App() {
   const [greeting, setGreeting] = useState('')
+  const [userAccount, setUserAccount] = useState(null)
+  const [amount, setAmount] = useState(0)
 
   const requestAccount = async() => {
-    await window.ethereum.request({ method: 'eth_requestAccounts' })
+    try {
+      await window.ethereum.request({ method: 'eth_requestAccounts' })
+    } catch(error) {
+      alert('Please install metamask and login first')
+    }
   }
 
   const fetchGreeting = async () => {
@@ -17,7 +23,7 @@ function App() {
       const contract = new ethers.Contract(greeterAddress, gAbi, signer)
       try {
         const data = await contract.greet()
-        console.log('Greet data: ', data)
+        alert(data)
       } catch(error) {
         console.log('Get Greeting Error: ', error)
       }
@@ -36,6 +42,29 @@ function App() {
     }
   }
 
+  const getBalance = async() => {
+    if(typeof window.ethereum !== 'undefined') {
+      const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' })
+      const provider = new ethers.providers.Web3Provider(window.ethereum)
+      const signer = provider.getSigner()
+      const contract = new ethers.Contract(tokenAddress, tAbi, signer)
+      const balance = await contract.balanceOf(accounts[0])
+      alert(balance.toString())
+    }
+  }
+
+  const sendTokens = async () => {
+    if(typeof window.ethereum !== 'undefined') {
+      await requestAccount()
+      const provider = new ethers.providers.Web3Provider(window.ethereum)
+      const signer = provider.getSigner()
+      const contract = new ethers.Contract(tokenAddress, tAbi, signer)
+      const transaction = await contract.transfer(userAccount, amount)
+      await transaction.wait()
+      alert(`${amount} Coins successfully sent to ${userAccount}`);
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -43,7 +72,10 @@ function App() {
         <button onClick={setG}>Set Greeting</button>
         <input onChange={e => setGreeting(e.target.value)} placeholder="Set greeting" />
         <br />
-
+        <button onClick={getBalance}>Get Balance</button>
+        <button onClick={sendTokens}>Send Tokens</button>
+        <input onChange={e => setUserAccount(e.target.value)} placeholder="Account ID" />
+        <input onChange={e => setAmount(e.target.value)} placeholder="Amount" />
       </header>
     </div>
   );
--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,4 +1,8 @@
 import greeterAbi from './Greeter.json'
+import tokenAbi from './Token.json'
 
-export const greeterAddress = '0xFe927de0cDC17F789aed19C43483825383Ca663F'
+export const greeterAddress = '0x9DfD6d4D3A48023439A0018D807B8cd404191E7B'
 export const gAbi = greeterAbi.abi
+
+export const tokenAddress = '0x441cD919be730B5542D499505d806481b1bA832B'
+export const tAbi = tokenAbi.abi
