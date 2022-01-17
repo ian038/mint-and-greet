c505fae2872b4f995735c4f8a70e91fb3a6e4356
ian038 <ianphua3@gmail.com>
2022-01-17 15:54:35 -0500

    Completed greeting portion of app

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,49 @@
+import React, { useState } from 'react'
 import './App.css';
+import { gAbi, greeterAddress } from './utils/constants';
+import { ethers } from 'ethers'
 
 function App() {
+  const [greeting, setGreeting] = useState('')
+
+  const requestAccount = async() => {
+    await window.ethereum.request({ method: 'eth_requestAccounts' })
+  }
+
+  const fetchGreeting = async () => {
+    if(typeof window.ethereum !== 'undefined') {
+      const provider = new ethers.providers.Web3Provider(window.ethereum)
+      const signer = provider.getSigner()
+      const contract = new ethers.Contract(greeterAddress, gAbi, signer)
+      try {
+        const data = await contract.greet()
+        console.log('Greet data: ', data)
+      } catch(error) {
+        console.log('Get Greeting Error: ', error)
+      }
+    }
+  }
+
+  const setG = async() => {
+    if(!greeting) return
+    if(typeof window.ethereum !== 'undefined') {
+      const provider = new ethers.providers.Web3Provider(window.ethereum)
+      const signer = provider.getSigner()
+      const contract = new ethers.Contract(greeterAddress, gAbi, signer)
+      const transaction = await contract.setGreeting(greeting)
+      await transaction.wait()
+      fetchGreeting()
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        Hello world
+        <button onClick={fetchGreeting}>Fetch Greeting</button>
+        <button onClick={setG}>Set Greeting</button>
+        <input onChange={e => setGreeting(e.target.value)} placeholder="Set greeting" />
+        <br />
+
       </header>
     </div>
   );
---
+++ client/src/utils/constants.js
@@ -0,0 +1,4 @@
+import greeterAbi from './Greeter.json'
+
+export const greeterAddress = '0xFe927de0cDC17F789aed19C43483825383Ca663F'
+export const gAbi = greeterAbi.abi
